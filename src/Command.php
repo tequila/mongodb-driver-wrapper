4ef6e74068c8f4d3b60daaaa1df74a0ca9d40efe
Petr Buchin <petrbuchyn@gmail.com>
2016-10-25 01:55:55 +0300

    Default Command implementation

---
+++ src/Command.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tequila\MongoDB;
+
+class Command implements CommandInterface
+{
+    /**
+     * @var object options to pass to @see \MongoDB\Driver\Command::__construct()
+     */
+    private $options;
+
+    /**
+     * @var bool
+     */
+    private $needsPrimaryServer;
+
+    /**
+     * @param array|object $options
+     * @param bool $needsPrimaryServer Whether this command needs to be executed on a primary server or not
+     */
+    public function __construct($options, $needsPrimaryServer)
+    {
+        $this->options = (object)$options;
+        $this->needsPrimaryServer = (bool)$needsPrimaryServer;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOptions(ServerInfo $serverInfo)
+    {
+        return $this->options;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function needsPrimaryServer()
+    {
+        return $this->needsPrimaryServer();
+    }
+}
--- src/OptionsProviderInterface.php
+++ src/OptionsProviderInterface.php
@@ -6,7 +6,7 @@ interface OptionsProviderInterface
 {
     /**
      * @param ServerInfo $serverInfo is passed for instance to resolve it's options depending on server version
-     * @return array
+     * @return array|object
      */
     public function getOptions(ServerInfo $serverInfo);
 }
