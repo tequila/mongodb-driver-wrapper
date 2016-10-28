ee9e3bc49829a825928e79e431c399780ac78480
Petr Buchin <petrbuchyn@gmail.com>
2016-10-28 03:40:36 +0300

    Method getOptions deleted from QueryInterface in favor of the OptionsProviderInterface

--- src/QueryInterface.php
+++ src/QueryInterface.php
@@ -8,9 +8,4 @@ interface QueryInterface extends OptionsProviderInterface
      * @return array|object
      */
     public function getFilter();
-
-    /**
-     * @inheritdoc
-     */
-    public function getOptions(ServerInfo $serverInfo);
 }
